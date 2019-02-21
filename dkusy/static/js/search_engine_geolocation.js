35d150fdbd4526e7a8311905682728c073ccc923
DanielKusy97 <daniel.kusy97@gmail.com>
2019-02-21 11:04:23 +0100

    Heroku

---
+++ dkusy/static/js/search_engine_geolocation.js
@@ -0,0 +1,77 @@
+$("#geolocation_button").click(function () {
+    origin = $("#id_origin");
+    origin.attr("placeholder", "");
+    origin.addClass('loaderbg');
+
+    if (navigator.geolocation)
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback_highAccuracy, {
+            maximumAge: 600000,
+            timeout: 5000,
+            enableHighAccuracy: true
+        });
+});
+
+function successCallback(position) {
+
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var coords = [latitude, longitude];
+    $.ajax({
+                    method: "GET",
+                    url: "https://nominatim.openstreetmap.org/reverse",
+                    data: {
+                        lat: position.coords.latitude,
+                        lon: position.coords.longitude,
+                        format: "jsonv2"
+                    },
+                      success: function(data) {
+                        origin.removeClass("loaderbg");
+                        origin.prop('disabled', true);
+                        $("#id_coordinates").val(coords);
+                        origin.val(data['address']['road'] + " " + data['address']['house_number']);
+                        let acc = position.coords.accuracy;
+                        console.log("Posisiton: \n%f,%f", latitude, longitude);
+                        console.log("Accuracy: ", acc);
+
+                      },
+                    error(data){
+                        error("Error");
+                    }
+                });
+}
+
+function errorCallback_highAccuracy(error) {
+    if (error.code === error.TIMEOUT)
+    {
+        // Attempt to get GPS loc timed out after 5 seconds,
+        // try low accuracy location
+        console.log("attempting to get low accuracy location");
+        navigator.geolocation.getCurrentPosition(
+               successCallback,
+               errorCallback_lowAccuracy,
+               {maximumAge:600000, timeout:10000, enableHighAccuracy: false});
+        return;
+    }
+
+    var msg = "<p>Can't get your location (high accuracy attempt). Error = ";
+    if (error.code === 1)
+        msg += "PERMISSION_DENIED";
+    else if (error.code === 2)
+        msg += "POSITION_UNAVAILABLE";
+    msg += ", msg = "+error.message;
+
+    console.log(msg);
+}
+
+
+function errorCallback_lowAccuracy(error) {
+    var msg = "<p>Can't get your location (low accuracy attempt). Error = ";
+    if (error.code === 1)
+        msg += "PERMISSION_DENIED";
+    else if (error.code = 2)
+        msg += "POSITION_UNAVAILABLE";
+    else if (error.code === 3)
+        msg += "TIMEOUT";
+    msg += ", msg = " + error.message;
+    console.log(msg);
+}
